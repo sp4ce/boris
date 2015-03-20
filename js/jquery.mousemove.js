7419f88a99dbdf747606b0780a95686288d65e21
Baptiste Pernet <bapt@sp4ce.net>
2015-03-20 17:30:36 -0500

    Improve documentation

--- js/jquery.mousemove.js
+++ js/jquery.mousemove.js
@@ -1,4 +1,4 @@
-// Event to detect the start and the end of the move move.
+// Event to detect the start and the end of the mouse move.
 (function ($) {
     var moving = false;
     var timeout;
@@ -6,23 +6,27 @@
 
     // Bind on mouse move event that is called each time the mouse is moved.
     $(document).on('mousemove', function (event) {
-        // The mouse is not moving, so start the moving process.
+        // The mouse was not moving, so trigger the start mouse move event.
         if (moving === false) {
-            // trigger the new event on event.target, so that it can bubble appropriately
+            // trigger the new event on event.target, so that it can bubble
+            // appropriately.
             $(event.target).trigger('mousemovestart');
             moving = true;
         }
 
-        // The timeout has been defined, so clear it.s
+        // The timeout has been defined previously,
+        // so clear it because the mouse is still moving.
         if (timeout !== undefined) {
             window.clearTimeout(timeout);
         }
 
         // Defined a timeout on the move of the mouse.
         timeout = window.setTimeout(function () {
-            // trigger the new event on event.target, so that it can bubble appropriately.
+            // trigger the new event on event.target, so that it can bubble
+            // appropriately.
             $(event.target).trigger('mousemoveend');
-            // Set the moving flag to false to trigger the start event the next time.
+            // Set the moving flag to false to trigger the start event the next
+            // time the mouse will start to move.
             moving = false;
         }, interval);
     });
